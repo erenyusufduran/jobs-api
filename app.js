4d31bbc039936e237ea2e21f5277f50591e7b6f2
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-09 22:32:33 +0300

    server initialize and project structure

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+// extra packages
+
+// routes
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
